f60e599c77999c5774189430c51eebb9e41cdc18
Alex Parkinson <okalex@gmail.com>
2015-09-02 20:33:34 -0700

    list/tree

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -168,6 +168,33 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def removeOdds(ints: List[Int]) = filter(ints)(_ % 2 == 0)
   def removeOddsLeft(ints: List[Int]) = filterLeft(ints)(_ % 2 == 0)
+
+  def addLists(as: List[Int], bs: List[Int]): List[Int] = (as, bs) match {
+    case (Nil, Nil) => Nil
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, addLists(xs, ys))
+  }
+
+  def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = (l1, l2) match {
+    case (Nil, Nil) => Nil
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+  }
+
+  def startsWith[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => false
+    case Cons(x, xs) => sub match {
+      case Nil => false
+      case Cons(y, Nil) => x == y
+      case Cons(y, ys) => if (x == y) startsWith(xs, ys) else false
+    }
+  }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    sup match {
+      case Nil => false
+      case Cons(x, xs) => 
+        if (startsWith(sup, sub)) true else hasSubsequence(xs, sub)
+    }
+  }
 }
 
 object ListModule {
@@ -176,44 +203,64 @@ object ListModule {
   def main(args: Array[String]): Unit = {
     println(s"List.x = ${List.x}")
 
-    val l = List(1, 2, 3, 4, 5)
+    val l1 = List(1, 2, 3, 4, 5)
     val l2 = List(6, 7, 8, 9, 10)
     val l3 = List(11, 12, 13, 14, 15)
     val l4 = List(16, 17, 18, 19, 20)
-    val ls = List(l, l2, l3, l4)
+    val ls = List(l1, l2, l3, l4)
+    val l = concat(ls)
 
     val ds = List[Double](1.0, 2.0, 3.0, 4.0, 5.0)
 
-    println(s"l = $l")
+    println(s"l1 = $l1")
 
-    println(s"tail(l) = ${tail(l)}")
-    println(s"drop(l, 3) = ${drop(l, 3)}")
-    println(s"dropWhile(l, x < 5) = ${dropWhile(l, (x: Int) => x < 5)}")
-    println(s"init(l) = ${init(l)}")
-    println(s"length(l) = ${length(l)}")
-    println(s"foldLeft(l, 0)(_ + _) = ${foldLeft(l, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
-    println(s"sumLeft(l) = ${sumLeft(l)}")
-    println(s"productLeft(l) = ${productLeft(l)}")
-    println(s"lengthLeft(l) = ${lengthLeft(l)}")
-    println(s"reverse(l) = ${reverse(l)}")
-    println(s"foldLeftFromRight(l, 0)(_ + _) = ${foldLeftFromRight(l, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
-    println(s"foldRightFromLeft(l, 0)(_ + _) = ${foldRightFromLeft(l, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
+    println(s"tail(l1) = ${tail(l1)}")
+    println(s"drop(l1, 3) = ${drop(l1, 3)}")
+    println(s"dropWhile(l1, x < 5) = ${dropWhile(l1, (x: Int) => x < 5)}")
+    println(s"init(l1) = ${init(l1)}")
+    println(s"length(l1) = ${length(l1)}")
+    println(s"foldLeft(l1, 0)(_ + _) = ${foldLeft(l1, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
+    println(s"sumLeft(l1) = ${sumLeft(l1)}")
+    println(s"productLeft(l1) = ${productLeft(l1)}")
+    println(s"lengthLeft(l1) = ${lengthLeft(l1)}")
+    println(s"reverse(l1) = ${reverse(l1)}")
+    println(s"foldLeftFromRight(l1, 0)(_ + _) = ${foldLeftFromRight(l1, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
+    println(s"foldRightFromLeft(l1, 0)(_ + _) = ${foldRightFromLeft(l1, 0){ (x, a) => println(s"eval $x: ${a + x}"); a + x }}")
 
-    println(s"append(l, l2) = ${append(l, l2)}")
-    println(s"appendFold(l, l2) = ${appendFold(l, l2)}")
+    println(s"append(l1, l2) = ${append(l1, l2)}")
+    println(s"appendFold(l1, l2) = ${appendFold(l1, l2)}")
     println(s"concat(ls) = ${concat(ls)}")
     println(s"concatLeft(ls) = ${concatLeft(ls)}")
 
-    println(s"addOne(l) = ${addOne(l)}")
+    println(s"addOne(l1) = ${addOne(l1)}")
     println(s"doublesToString(ds) = ${doublesToString(ds)}")
-    println(s"map(l)(_ + 1) = ${map(l)(_ + 1)}")
-    println(s"mapLeft(l)(_ + 1) = ${mapLeft(l)(_ + 1)}")
+    println(s"map(l1)(_ + 1) = ${map(l1)(_ + 1)}")
+    println(s"mapLeft(l1)(_ + 1) = ${mapLeft(l1)(_ + 1)}")
 
     println(s"removeOdds(l) = ${removeOdds(l)}")
     println(s"removeOddsLeft(l) = ${removeOddsLeft(l)}")
 
-    println(s"flatMap(l)(x => List(x + 1, x + 2, x + 3)) = ${flatMap(l)(x => List(x + 1, x + 2, x + 3))}")
+    println(s"flatMap(l1)(x => List(x + 1, x + 2, x + 3)) = ${flatMap(l1)(x => List(x + 1, x + 2, x + 3))}")
     println(s"filterFromFlatMap(l)(_ % 2 == 0) = ${filterFromFlatMap(l)(_ % 2 == 0)}")
+
+    println(s"addLists(l1, l2) = ${addLists(l1, l2)}")
+    println(s"zipWith(l1, l2)(_ + _) = ${zipWith(l1, l2)(_ + _)}")
+
+    println("\nstartsWith")
+    println(s"${startsWith(l, l1)}")
+    println(s"${startsWith(l, l2)}")
+    println(s"${startsWith(l, l3)}")
+    println(s"${startsWith(l, l4)}")
+    println(s"${startsWith(l, l)}")
+
+    println("\nhasSubsequence")
+    println(s"${hasSubsequence(l, l1)}")
+    println(s"${hasSubsequence(l, l2)}")
+    println(s"${hasSubsequence(l, l3)}")
+    println(s"${hasSubsequence(l, l4)}")
+    println(s"${hasSubsequence(l, l)}")
+    println(s"${hasSubsequence(l, append(l1, l3))}")
+    println(s"${hasSubsequence(l, append(l2, l4))}")
 
   }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,53 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+	def size[A](t: Tree[A]): Int = t match {
+		case Leaf(_) => 1
+		case Branch(l, r) => size(l) + size(r) + 1
+	}
 
+	def maximum(t: Tree[Int]): Int = t match {
+		case Leaf(v) => v
+		case Branch(l, r) => maximum(l).max(maximum(r))
+	}
 
+	def depth[A](t: Tree[A]): Int = t match {
+		case Leaf(_) => 0
+		case Branch(l, r) => depth(l).max(depth(r)) + 1
+	}
 
+	def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+		case Leaf(a) => Leaf(f(a))
+		case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+	}
+
+	def fold[A]
+
+}
+
+object TreeModule {
+	import Tree._
+
+	def main(args: Array[String]): Unit = {
+		println("Welcome to TreeModule")
+
+		val t1 = Branch(
+					Branch(
+						Leaf(1),
+						Branch(
+							Leaf(2),
+							Branch(
+								Leaf(3),
+								Leaf(4)
+							)
+						)
+					),
+					Leaf(5)
+				)
+
+		println(s"size(t1) = ${size(t1)}")
+		println(s"maximum(t1) = ${maximum(t1)}")
+		println(s"depth(t1) = ${depth(t1)}")
+		println(s"map(t1)(_ + 1) = ${map(t1)(_ + 1)}")
+	}
 }
